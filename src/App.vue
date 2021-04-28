c6ed8e44694b27616124aa0829507599e1b9893d
TawhidSajib <tauhidulsajib34@gmail.com>
2021-04-28 19:39:40 +0600

    code updated

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <HelloWorld></HelloWorld>
+    <portfolio></portfolio>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import portfolio from "./components/portfolio.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    portfolio,
   },
 };
 </script>
--- src/components/portfolio.vue
+++ src/components/portfolio.vue
@@ -179,45 +179,6 @@ export default {
       myJson: user,
     };
   },
-  // mounted() {
-  //   fetch("user.json")
-  //     .then((res) => {
-  //       return res.json();
-  //     })
-  //     .then((data) => {
-  //       let serviceColumn = document.querySelectorAll(".serviceColumn");
-  //       data.data.forEach((user, index) => {
-  //         serviceColumn[index].innerHTML += `
-  //             <h1 class="service-header">${user.header}</h1>
-  //             <p class="service-para">${user.paragraph}</p>
-
-  //             `;
-  //       });
-
-  //       let workColumn = document.querySelectorAll(".workColumn");
-  //       data.images.forEach((user, index) => {
-  //         workColumn[
-  //           index
-  //         ].innerHTML += `<img class="workImg" src="${user.image}" height="100%" width="100%">`;
-  //       });
-
-  //       let name = document.querySelector(".name");
-  //       let phone = document.querySelector(".phone");
-  //       let email = document.querySelector(".email");
-  //       data.info.forEach((user) => {
-  //         name.innerHTML += `I am <strong>${user.name}</strong>`;
-  //         phone.innerHTML += `<span>Phone:</span> ${user.phone} `;
-  //         email.innerHTML += `<span>Email:</span> ${user.email} `;
-  //       });
-
-  //       let skillName = document.querySelectorAll(".skillName");
-  //       let skill = document.querySelectorAll(".skill");
-  //       data.skills.forEach((user, index) => {
-  //         skillName[index].innerHTML += `${user.name}`;
-  //         skill[index].innerHTML += `${user.progress}`;
-  //       });
-  //     });
-  // },
   methods: {
     myFunction() {
       var x = document.querySelector(".nav-list");
@@ -227,9 +188,6 @@ export default {
         x.style.display = "block";
       }
     },
-    // getImage(path) {
-    //   return require(path);
-    // },
   },
 };
 </script>
