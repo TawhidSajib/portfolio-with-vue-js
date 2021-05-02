a7b946fb34dec6dd4720550045938a271ca4ce7b
TawhidSajib <tauhidulsajib34@gmail.com>
2021-05-02 12:34:19 +0600

    import images and used

--- src/components/portfolio.vue
+++ src/components/portfolio.vue
@@ -124,18 +124,9 @@
         </header>
 
         <div class="row wrap">
-          <div
-            class="col-33"
-            v-for="(user, index) in myJson.images"
-            :key="index"
-          >
+          <div class="col-33" v-for="(image, index) in images" :key="index">
             <figure class="workColumn">
-              <img
-                class="workImg"
-                :src="user.image"
-                height="100%"
-                width="100%"
-              />
+              <img class="workImg" :src="image" height="100%" width="100%" />
             </figure>
           </div>
         </div>
@@ -176,20 +167,20 @@
 </template>
 <script>
 import user from "@/assets/json/user.json";
+import image1 from "@/assets/images/work1.jpg";
+import image2 from "@/assets/images/work2.jpg";
+import image3 from "@/assets/images/work3.jpg";
+import image4 from "@/assets/images/work4.jpg";
+import image5 from "@/assets/images/work5.jpg";
+import image6 from "@/assets/images/work6.jpg";
 export default {
   data() {
     return {
       myJson: user,
       display: true,
+      images: [image1, image2, image3, image4, image5, image6],
     };
   },
 };
 </script>
-<style scoped>
-.displayBlock {
-  display: block;
-}
-.displayNone {
-  display: none;
-}
-</style>>
+
